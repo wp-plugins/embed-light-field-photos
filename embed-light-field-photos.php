8e068c2a89ccb03211c92faac1cea9fe4f2ff251
slash0190 <slash0190@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-01 22:59:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/embed-light-field-photos/tags/1.0@513215 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ embed-light-field-photos.php
@@ -0,0 +1,54 @@
+<?php  
+/* 
+Plugin Name: Embed Light Field Photos
+Plugin URI: http://www.lightfieldphotobook.com/wordpress-plugins/embed-light-field-photos
+Description: Simply drop URL of a light field photo from Lytro.com in your post and the photo will be displayed there (the same as you do with Youtube videos).
+Version: 1.0 
+Author: Light-Field Photo Book
+Author URI: http://www.lightfieldphotobook.com
+*/
+
+/*  Copyright 2012 http://www.lightfieldphotobook.com (e-mail: admin@lightfieldphotobook.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+add_filter('the_content', 'embed_light_field_photo');  
+function embed_light_field_photo($text) {  
+preg_match_all("/(http:\/\/)(www.lytro.com|lytro.com)(\/living-pictures\/)(.*)/", $text, $matches, PREG_SET_ORDER);
+foreach ($matches as $val) {
+$pieces = explode("/", $val[0]);
+$photo_id = ereg_replace ('<', '', $pieces[4]);
+$text = ereg_replace($val[0], '<iframe src="http://www.lytro.com/living-pictures/'.$photo_id.'/embed?utm_source=Embed&amp;utm_medium=EmbedLink" scrolling="no" width="500" frameborder="0" height="515"></iframe>', $text);
+}
+return $text;
+}
+
+function embed_light_field_photo_options_page() {
+?>
+   <div class="wrap">
+   	<h2><?php _e('Embed Light-Field Photos', HI_TEXTDOMAIN); ?></h2>
+<br />
+Thank you for using Embed Light Field Photos plugin. Visit <a href="http://www.lightfieldphotobook.com" target="_blank">www.lightfieldphotobook.com</a> for more light field photography related news, information and free services.
+   </div>
+<?php
+}
+
+function embed_light_field_photo_admin_menu() {
+   add_options_page(__('Light-Field Photos', HI_TEXTDOMAIN), __('Light-Field Photos', HI_TEXTDOMAIN), 'manage_options', basename(__FILE__), 'embed_light_field_photo_options_page');
+}
+
+add_action('admin_menu', 'embed_light_field_photo_admin_menu');
+?>
---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== Embed Light-Field Photos ===
+Contributors: Slash
+Tags: light field photos, light field photography, light field camera, living pictures, lytro
+Requires at least: 2.0
+Tested up to: 3.3.1
+Stable tag: 3.3.1
+
+Embed Light Field Photos makes it easy to include photos from the www.lytro.com/living-pictures website in your posts and pages.
+
+== Description ==
+
+You can easily add the photos from the lytro.com website to your posts and pages. All you have to do is copy the URL of the photo and drop it into your post, the same as you do with Youtube videos. The url is automatically changed so the photo is displayed on your blog.
+
+You can find more information about the plugin at http://www.lightfieldphotobook.com/wordpress-plugins/embed-light-field-photos
+
+== Installation ==
+
+Upload the Embed Light-Field Photos plugin to your blog, Activate it and you are done.
+
+== Screenshots ==
+
+1. Example of URLs pasted in the post or page
+2. URLs converted into photos on your blog
+
